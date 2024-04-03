3ae7424f627958877c18f1015ea68de3f72a952b
sushant babu prasai <sushantprasaii7@gmail.com>
2024-04-03 22:27:13 +0545

    dummy class component

---
+++ src/classics/firstClass.jsx
@@ -0,0 +1,13 @@
+import  { Component } from "react";
+
+class MyClassComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello world this is my first class component</h1>
+      </div>
+    );
+  }
+}
+
+export default MyClassComponent
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 import './index.css'
-import RecipeApp from './react-recipe'
+import MyClassComponent from './classics/firstClass'
+// import RecipeApp from './react-recipe'
 // import UseCallback from './bestPractices/hooks/useCallback'
 // import UseStateHook from './bestPractices/hooks/hooks.jsx'
 // import UsememoHook from './bestPractices/hooks/usememo.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RecipeApp/>
+    {/* <RecipeApp/> */}
+    <MyClassComponent/>
     {/* <App /> */}
     {/* <UseStateHook/> */}
     {/* <UseCallback/> */}
