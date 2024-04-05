46b8f54300a24d02ccb19a248a517876d3e58bd6
sushant babu prasai <sushantprasaii7@gmail.com>
2024-04-05 11:39:04 +0545

    rendering three colored buttons

--- src/classics/firstClass.jsx
+++ src/classics/firstClass.jsx
@@ -1,13 +1,37 @@
-import  { Component } from "react";
+import { Component } from "react";
 
 class MyClassComponent extends Component {
+  buttons = ["red", "green", "blue"];
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      name: "hello world",
+      color: ["#990000", "#009900", "#000099"],
+    };
+  }
+  handleColorChange = (color) => {
+    this.setState({ color: color }); // Update state with selected color
+    console.log("change color");
+  };
+
   render() {
+    const { color } = this.state;
     return (
       <div>
-        <h1>hello world this is my first class component</h1>
+        {this.buttons.map((btn, key) => (
+          <button
+            key={key}
+            className="text-[24px] mr-2 mb-2 px-4 py-2 rounded"
+            style={{ backgroundColor: color[key] }}
+            onClick={() => this.handleColorChange(btn)}
+          >
+            {btn}
+          </button>
+        ))}
       </div>
     );
   }
 }
 
-export default MyClassComponent
+export default MyClassComponent;
